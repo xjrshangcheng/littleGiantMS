6da7a2088817225b5cc7dc53a1bd0d1a0d9d8e2d
luojinghui <939035243@qq.com>
2015-08-14 23:57:22 +0800

    add ueditor config

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var app = express();
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 
+var path = require('path');
+var ueditor = require("ueditor");
+
 app.set('view engine', 'jade');
 
 app.use(bodyParser.urlencoded({
@@ -12,6 +15,32 @@ app.use(bodyParser.urlencoded({
 app.use(cookieParser());
 app.use(express.static("public"));
 app.use(express.static("bower_components"));
+
+// app.use(express.static(path.join(__dirname, 'public')));
+// app.set('views', path.join(__dirname, 'views'));
+
+app.use("/ueditor/ue", ueditor(path.join(__dirname, 'public'), function(req, res, next) {
+    // ueditor 客户发起上传图片请求
+    if (req.query.action === 'uploadimage') {
+        var foo = req.ueditor;
+
+        var imgname = req.ueditor.filename;
+
+        var img_url = '/images/goods/' ;
+        res.ue_up(img_url); //你只要输入要保存的地址 。保存操作交给ueditor来做
+    }
+    //  客户端发起图片列表请求
+    else if (req.query.action === 'listimage') {
+        var dir_url = '/images/goods/';
+        res.ue_list(dir_url); // 客户端会列出 dir_url 目录下的所有图片
+    }
+    // 客户端发起其它请求
+    else {
+        // console.log('config.json')
+        res.setHeader('Content-Type', 'application/json');
+        res.redirect('/ueditor/nodejs/config.json');
+    }
+}));
 
 // var index = require("./router/index");
 // app.get('/', index);
