7182566ad25327677d64fd7ff9f7354a6d9ba0c9
luojinghui <939035243@qq.com>
2015-08-17 14:05:33 +0800

    add table goods model

---
+++ models/goods.js
@@ -0,0 +1,24 @@
+var sequelize = require('../db-connection/connection');
+
+module.exports = function(sequelize, DataTypes) {
+    var Goods = sequelize.define('goods', {
+        id: DataTypes.INTEGER,
+        name: DataTypes.STRING,
+        info: DataTypes.STRING,
+        price: DataTypes.STRING,
+        standard_one: DataTypes.STRING,
+        standard_two: DataTypes.STRING,
+        sales: DataTypes.STRING,
+        detail: DataTypes.INTEGER,
+        type: DataTypes.STRING,
+        img: DataTypes.STRING,
+        recommend: DataTypes.STRING,
+        more_img: DataTypes.STRING,
+        special_img: DataTypes.STRING,
+        detail_img: DataTypes.STRING
+    }, {
+        freezeTableName: true,
+        timestamps: false
+    });
+    return Goods;
+};
