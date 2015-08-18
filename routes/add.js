44ec44e84715eb3808315f5fec49daa101597594
luojinghui <939035243@qq.com>
2015-08-18 16:55:06 +0800

    test add function

--- routes/add.js
+++ routes/add.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models');
+var Goods = models.goods;
+var add = require('../service/add');
+
 
 router.get('/', function(req, res) {
-  res.render('add');
+    res.render('add');
 });
+
+router.post('/', function(req, res) {
+    console.log(typeof add);
+    add(req, res);
+})
 
 module.exports = router;
