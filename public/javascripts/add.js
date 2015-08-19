f6bf4713e34429ad2b3f2c82be610160410a0461
luojinghui <939035243@qq.com>
2015-08-19 10:45:52 +0800

    add input listen for can't insert no number filed

--- public/javascripts/add.js
+++ public/javascripts/add.js
@@ -2,9 +2,6 @@ $("#goods-confirm").on("click",function() {
     var editContent = UE.getEditor('editor').getContent();
     var content = detail(editContent);
     var goodsName = $("#goods-name").prop("value");
-
-
-
     var goodsInfo = $("#goods-simple-info").prop("value");
     var goodsStandards = $("#goods-standards").prop("value");
     var goodsRemark = $("#goods-remark").prop("value");
@@ -13,29 +10,27 @@ $("#goods-confirm").on("click",function() {
     var number = $("#number").prop("value");
     var id = $("#code").prop("value");
 
-    $.post("/add",{
-        name : goodsName,
-        info : goodsInfo,
-        standards : goodsStandards,
-        remark : goodsRemark,
-        unit : goodsUnit,
-        price : price,
-        number : number,
-        id : id,
-        detail : content
-    }, function(data) {
-        console.log(data);
-        if(data.status === "200") {
-            alert("商品添加成功");
-        } else if(data.status === "400") {
-            alert(data.message);
-        } else {}
-    });
+    if (judgeContent(goodsName, goodsUnit, price, number, id)) {
+        $.post("/add", {
+            name: goodsName,
+            info: goodsInfo,
+            standards: goodsStandards,
+            remark: goodsRemark,
+            unit: goodsUnit,
+            price: price,
+            number: number,
+            id: id,
+            detail: content
+        }, function(data) {
+            console.log(data);
+            if (data.status === "200") {
+                alert("商品添加成功");
+            } else if (data.status === "400") {
+                alert(data.message);
+            } else {}
+        });
+    } else {}
 })
-
-$("#goods-name").blur(function(){
-
-});
 
 var detail = function(editContent) {
     var Splitstring = editContent.split('src="');
@@ -50,3 +45,51 @@ var detail = function(editContent) {
     })
     return joinString;
 }
+
+var judgeContent = function(goodsName,goodsUnit,price,number,id) {
+    if(goodsName === '') {
+        executeJudge("#goods-name");
+    } else if (goodsUnit === ''){
+        executeJudge("#goods-market-unit");
+    }  else if (price === ''){
+        executeJudge("#price");
+    }  else if (number === ''){
+        executeJudge("#number");
+    }  else if (id === ''){
+        executeJudge("#code");
+    } else {
+        return true;
+    }
+}
+
+var executeJudge = function(selector) {
+    $(selector).focus();
+    $(selector).popover('show');
+    window.setTimeout(function() { $(selector).popover('destroy') }, 2100);
+    return false;
+}
+
+$("#price, #code, #number").on("keydown", function() {
+    $(this)[0].value = $(this)[0].value.replace(/[^\d.]/g,"");
+    $(this)[0].value = $(this)[0].value.replace(/^\./g,"");
+    $(this)[0].value = $(this)[0].value.replace(/\.{2,}/g,".");
+    $(this)[0].value = $(this)[0].value.replace(".","$#$").replace(/\./g,"").replace("$#$",".");
+});
+
+$("#price").blur(function() {
+    if(!isNaN($("#price")[0].value) === false) {
+        $("#price")[0].value = $("#price")[0].value.substring(0,$("#price")[0].value.length - 1);
+    }
+});
+
+$("#code").blur(function() {
+    if(!isNaN($("#code")[0].value) === false) {
+        $("#code")[0].value = $("#code")[0].value.substring(0,$("#code")[0].value.length - 1);
+    }
+});
+
+$("#number").blur(function() {
+    if(!isNaN($("#number")[0].value) === false) {
+        $("#number")[0].value = $("#number")[0].value.substring(0,$("#number")[0].value.length - 1);
+    }
+});
