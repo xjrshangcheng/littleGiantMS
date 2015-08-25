a429a9b1e439a3afcf005419e87ff9abf9b74c01
luojinghui <939035243@qq.com>
2015-08-25 15:23:44 +0800

    use cotroller function to replace primary function

--- routes/add.js
+++ routes/add.js
@@ -1,15 +1,12 @@
 var express = require('express');
 var router = express.Router();
-var models = require('../models');
-var Goods = models.goods;
-var add = require('../service/add');
+var addController = require('../controller/add-controller');
+var add = new addController();
 
 router.get('/', function(req, res) {
     res.render('add');
 });
 
-router.post('/', function(req, res) {
-    add(req, res);
-})
+router.post('/', add.add_goods);
 
 module.exports = router;
