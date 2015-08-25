82841bd184013279f3fe584fe669b826037cf3ef
wanglitao <wanglitao1202@outlook.com>
2015-08-25 13:11:45 +0800

    wlt imolement the login authentication function

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,1 +1,9 @@
 $('#username').focus();
+
+$(function() {
+    $('#submit').on('click', function() {
+        if($('#username').prop('value') === 'admin' & $('#password').prop('value')=== 'admin' ) {
+            javascript:window.open('http://localhost:3001/goods_query');
+        }
+    })
+});
