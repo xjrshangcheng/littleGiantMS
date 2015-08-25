dd28cf58a315d5af4f7af18ba9d22a6332e0b04e
maming <864845434@qq.com>
2015-08-25 17:40:29 +0800

    finish query for price

--- models/goods.js
+++ models/goods.js
@@ -1,3 +1,31 @@
+//var sequelize = require('../db-connection/connection');
+//
+//module.exports = function(sequelize, DataTypes) {
+//    var Goods = sequelize.define('goods', {
+//        id: DataTypes.INTEGER,
+//        name: DataTypes.STRING,
+//        info: DataTypes.STRING,
+//        price: DataTypes.STRING,
+//        standard_one: DataTypes.STRING,
+//        standard_two: DataTypes.STRING,
+//        sales: DataTypes.STRING,
+//        detail: DataTypes.STRING,
+//        type: DataTypes.STRING,
+//        img: DataTypes.STRING,
+//        recommend: DataTypes.STRING,
+//        more_img: DataTypes.STRING,
+//        special_img: DataTypes.STRING,
+//        detail_img: DataTypes.STRING,
+//        remark: DataTypes.STRING,
+//        barcode: DataTypes.STRING,
+//        status: DataTypes.STRING,
+//        inventory: DataTypes.INTEGER
+//    }, {
+//        freezeTableName: true,
+//        timestamps: false
+//    });
+//    return Goods;
+//};
 var sequelize = require('../db-connection/connection');
 
 module.exports = function(sequelize, DataTypes) {
@@ -22,7 +50,23 @@ module.exports = function(sequelize, DataTypes) {
         inventory: DataTypes.INTEGER
     }, {
         freezeTableName: true,
-        timestamps: false
+        timestamps: false,
+        classMethods: {
+            findAllMessage : function(barcode, name, priceDown, priceUp) {
+                return Goods.findAll({ where: {
+                    barcode: {
+                        $like: barcode + '%'
+                    },
+                    name : {
+                        $like : '%' + name + '%'
+                    },
+                    price : {
+                        $between : [priceDown, priceUp]
+                    }
+                }
+                })
+            }
+        }
     });
     return Goods;
 };
--- routes/goods_query.js
+++ routes/goods_query.js
@@ -12,42 +12,31 @@ router.get('/:input', function(req, res) {
     var urlParameter = input.split('&');
     var name = urlParameter[0];
     var barcode = urlParameter[1];
-    var priceDown = parseInt(urlParameter[2]);
-    var priceUp = parseInt(urlParameter[3]);
+    var priceDown = urlParameter[2];
+    var priceUp = urlParameter[3];
+    priceDown === '' ?  priceDown = 0 : priceDown = parseInt(urlParameter[2]);
+    priceUp === '' ? priceUp = Number.MAX_VALUE : priceUp = parseInt(urlParameter[3]);
     var queryResult = [];
     var status;
     var message;
-    Goods.findAll({
-            where: {
-                //barcode: {
-                //    $like: barcode + '%'
-                //},
-                //name : {
-                //    $like : '%'+name
-                //},
-                price : {
-                    $between : [priceDown, priceUp]
-                }
-            }
-        }).then(function(data) {
-            if (data.length > 0) {
-                data.forEach(function(val) {
-                    queryResult.push(val.dataValues);
-                });
-                status = 200;
-                message = 'ok';
-            } else {
-                status = 100;
-                message = 'error';
-            }
-        })
-        .done(function() {
-            res.send({
-                    status: status,
-                    data: queryResult,
-                    message: message
-                })
-        })
+    Goods.findAllMessage(barcode, name, priceDown, priceUp).then(function (data) {
+        if (data.length > 0) {
+            data.forEach(function (val) {
+                queryResult.push(val.dataValues);
+            });
+            status = 200;
+            message = 'ok';
+        } else {
+            status = 100;
+            message = 'error';
+        }
+    }).done(function () {
+        res.send({
+            status: status,
+            data: queryResult,
+            message: message
+        });
+    })
 })
 
 module.exports = router;
