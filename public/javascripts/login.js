91d4ae0f7cf697ad05dce965df423d7c99154ebe
wanglitao <wanglitao1202@outlook.com>
2015-08-27 16:15:04 +0800

    wlt the optimization of the code

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -2,43 +2,31 @@ $('#username').focus();
 
 $(function() {
     $('#submit').on('click', function() {
-        var inputName = $('#username').prop('value');
-        var inputPwd = $('#password').prop('value');
-
-        $.ajax({
-            url: '/login',
-            type: 'post',
-            data: {
-                inputName: inputName,
-                inputPwd: inputPwd
-            },
-            success: function(result) {
-                if (result.data === 'ok') {
-                    $(location).attr('href', '/goods_query');
-                }
-            }
-        })
+        check();
     })
 });
 
-
-$('html').bind('keydown',function(e){
-      if(e.keyCode === 13){
-          var inputName = $('#username').prop('value');
-          var inputPwd = $('#password').prop('value');
-
-          $.ajax({
-              url: '/login',
-              type: 'post',
-              data: {
-                  inputName: inputName,
-                  inputPwd: inputPwd
-              },
-              success: function(result) {
-                  if (result.data === 'ok') {
-                      $(location).attr('href', '/goods_query');
-                  }
-              }
-          })
-      }
+$('html').bind('keydown',function(event){
+    if(event.keyCode === 13){
+        check();
+    }
  });
+
+function check() {
+    var inputName = $('#username').prop('value');
+    var inputPwd = $('#password').prop('value');
+
+    $.ajax({
+        url: '/login',
+        type: 'post',
+        data: {
+            inputName: inputName,
+            inputPwd: inputPwd
+        },
+        success: function(result) {
+            if (result.data === 'ok') {
+                $(location).attr('href', '/goods_query');
+            }
+        }
+    })
+}
