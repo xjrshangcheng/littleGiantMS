4417ae7193f35e78fe2e6c5e74358ba2c21d7607
wanglitao <wanglitao1202@outlook.com>
2015-08-27 10:00:13 +0800

    wlt add the keyboard enter to monitor events

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -13,11 +13,32 @@ $(function() {
                 inputPwd: inputPwd
             },
             success: function(result) {
-                console.log(result.data);
                 if (result.data === 'ok') {
                     $(location).attr('href', '/goods_query');
                 }
             }
         })
     })
-})
+});
+
+
+$('html').bind('keydown',function(e){
+      if(e.keyCode === 13){
+          var inputName = $('#username').prop('value');
+          var inputPwd = $('#password').prop('value');
+
+          $.ajax({
+              url: '/login',
+              type: 'post',
+              data: {
+                  inputName: inputName,
+                  inputPwd: inputPwd
+              },
+              success: function(result) {
+                  if (result.data === 'ok') {
+                      $(location).attr('href', '/goods_query');
+                  }
+              }
+          })
+      }
+ });
